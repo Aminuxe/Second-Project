80fc1d6bced04474b80f5142f0fa1821b7bdf1b6
Aminuxe <115246185+Aminuxe@users.noreply.github.com>
2022-10-27 19:24:47 +0100

    Add files via upload
    Micro service
    Bank account management

---
+++ Bank-account-service/src/main/java/org/sid/bankaccountservice/service/AccountService.java
@@ -0,0 +1,13 @@
+package org.sid.bankaccountservice.service;
+
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+import org.springframework.stereotype.Service;
+
+
+public interface AccountService {
+    public BankAccountResponseDTO addAccount (BankAccountRequestDTO bankAccountDTO) ;
+
+    BankAccountResponseDTO updateAccount(String id, BankAccountRequestDTO bankAccountDTO);
+}
